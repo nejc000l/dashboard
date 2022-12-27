8052244890b0e0112acef89c25a210c157ebd677
nejc000l <nejc.lampic@gmail.com>
2022-12-27 22:21:47 +0100

    the first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {ColorModeContext,useMode} from './theme'
+import { CssBaseline,ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar'
+import { useState } from 'react';
+
 function App() {
-  return (
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);  
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
     <div className="app">
-     
+      <main className="content">
+        <Topbar/>
+      </main>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
